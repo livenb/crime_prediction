a719bf9a8e6894a4576ca9a68118d56b0a44e5a0
livenb <livenb666@gmail.com>
2016-08-15 09:43:29 -0700

    update

--- build_map.py
+++ build_map.py
@@ -35,20 +35,41 @@ def get_month_df(df):
     lamo = df.groupby(['Year', 'Month', 'ZIP', 'CrimeCat'])['Unnamed: 0'].count()
     lamo = lamo.unstack().reset_index()
     lamo = lamo.fillna(0)
+    return lamo 
 
-    X_mo = lamo.drop(['Year', 'ZIP'], axis=1).values
-    return X_mo
+
+def get_quater(x):
+    if x < 4:
+        return 1
+    elif 4 <= x < 7:
+        return 2
+    elif 7 <= x < 10:
+        return 3
+    else:
+        return 4
+
+
+def get_quater_df(df):
+    laqt = get_month_df(df)
+    laqt['Quater'] = laqt['Month'].apply(get_quater)
+    laqt = laqt.drop('Month', axis=1).groupby(['Year', 'Quater', 'ZIP']).sum()
+    laqt = laqt.reset_index()
+    return laqt
+
 
 def build_nmf_all(X, k=5):
+    scaler = MinMaxScaler()
+    X_sca = scaler.fit_transform(X)
     nmfModel = NMF(n_components=k)
-    W = nmfModel.fit_transform(X)
+    W = nmfModel.fit_transform(X_sca)
     H = nmfModel.components_
     print 'NMF done!'
-    plot_heatmap(H.T, k)
-    return W, H
+    plot_heatmap(H.T, k=k)
+    labelsNMF = W.argmax(axis=1)
+    return W, H, labelsNMF, nmfModel
 
-def plot_heatmap(data, k):
-    fig, ax = plt.subplots(figsize = (8,8))
+def plot_heatmap(data, title=None, k=5):
+    fig, ax = plt.subplots(figsize = (12, 8))
     heatmap = ax.pcolor(data, cmap=plt.cm.Blues)
 
     # put the major ticks at the middle of each cell
@@ -62,6 +83,8 @@ def plot_heatmap(data, k):
     ax.set_xticklabels(classLabel, minor=False)
     ax.set_yticklabels(crimes.values(), minor=False)
     ax.set_title('Heatmap of Lattent Feature')
+    if title != None:
+        plt.savefig(title)
     # plt.show()
 
 
@@ -133,7 +156,7 @@ def cmap_discretize(cmap, N):
 def prep_df(df, W, labels):
     W_df =  pd.DataFrame(W)
     for col in W_df.columns:
-        W_df.loc[:, col] = Natural_Breaks(W_df.loc[:, col], k=10).yb
+        W_df.loc[:, col] = Natural_Breaks(W_df.loc[:, col], k=5).yb
     df['class'] = labels + 1
     df = pd.concat((df.drop(range(1,11), axis=1), W_df), axis=1)
     df['ZIP'] = df['ZIP'].apply(int).apply(str)
@@ -152,14 +175,14 @@ def get_df_map(df, m):
 #     print df_map.head()
     return df_map
 
-def build_map_nmf(df_map, m, coords, yr, title):
+def build_map_nmf(df_map, m, coords, info, title):
     # plt.clf()
     fig = plt.figure()
     ax = fig.add_subplot(111, axisbg='w', frame_on=True)
     # draw wards with grey outlines
     norm = Normalize()
     cmaps = []
-    colors = ['Blues', 'Greens', 'Oranges', 'Reds', 'Purples']
+    colors = ['Blues', 'Reds', 'Greens', 'Purples', 'Oranges']
     for i in xrange(5):
         color = colors[i]
         cmap = plt.get_cmap(color)
@@ -167,11 +190,10 @@ def build_map_nmf(df_map, m, coords, yr, title):
         pc.set_facecolor(cmap(norm(df_map.loc[(df_map['class'] == i+1), i].values)))
         ax.add_collection(pc)
     pc = PatchCollection(df_map[df_map['class'] == 0]['patches'], match_original=True, alpha=0.2)
-    pc.set_facecolor('Grey')
+    pc.set_facecolor('grey')
     ax.add_collection(pc)
-    x, y = m(coords[0], coords[1] + 1.0)
-    info = 'Los Angeles Commuties\n Categoried by Crime\n   {}'.format(yr)
-    details = plt.annotate(info, xy=(x, y), size=18, color='#555555')
+    x, y = m(coords[0] + 0.05, coords[1] + 1.0)
+    details = plt.annotate(info, xy=(x, y), size=22, color='#555555')
     # Draw a map scale
     m.drawmapscale(
         coords[0] + 0.2, coords[1] + 0.95,
@@ -189,27 +211,91 @@ def build_map_nmf(df_map, m, coords, yr, title):
 def build_map_yr(data):
     layr = get_year_df(data)
     X_yr = layr.drop(['Year', 'ZIP'], axis=1).values
-    scaler = MinMaxScaler()
-    X_sca = scaler.fit_transform(X_yr)
-    W, H = build_nmf_all(X_sca)
-    labelsNMF = W.argmax(axis=1)
-    # years = layr['Year']
-    # yrs = sorted(years.unique())
+    W, H, labelsNMF, nmfModel = build_nmf_all(X_yr)
     m, coords = get_basemap(LA_shapefile)
     new_df = prep_df(layr, W, labelsNMF)
     for yr in xrange(2004, 2016):
         print yr
         df_map = get_df_map(new_df[new_df['Year'] == yr], m)
         title = 'img/la-map-{}.png'.format(yr)
-        build_map_nmf(df_map, m, coords, yr, title)
+        info = 'Los Angeles Commuties\nCategoried by Crime\n   {0}'.format(yr)
+        build_map_nmf(df_map, m, coords, info, title)
+
+
+def build_map_month_yrbased(data):
+    layr = get_year_df(data)
+    X_yr = layr.drop(['Year', 'ZIP'], axis=1).values
+    Wyr, H, labelsNMFyr, nmfModel = build_nmf_all(X_yr)
+    lamo = get_month_df(data)
+    X_mo = lamo.drop(['Year', 'Month', 'ZIP'], axis=1).values
+    print 'Transfer Monthly Class'
+    W = nmfModel.transform(X_mo)
+    labelsNMF = W.argmax(axis=1)
+    print 'build base map'
+    m, coords = get_basemap(LA_shapefile)
+    print 'build new dataframe'
+    new_df = prep_df(lamo, W, labelsNMF)
+    for yr in xrange(2004, 2017):
+        for mo in xrange(1, 13):
+            print yr, mo
+            df_map = get_df_map(new_df[(new_df['Year'] == yr) |
+                                (new_df['Month'] == mo)], m)
+            if mo < 10:
+                title = 'img/la-map-{0}0{1}.png'.format(yr, mo)
+            else:
+                title = 'img/la-map-{0}{1}.png'.format(yr, mo)
+            info = 'Los Angeles Commuties\nCategoried by Crime\n{0}--{1}'.format(yr, mo)
+            build_map_nmf(df_map, m, coords, info, title)
 
 
 def build_map_month(data):
     lamo = get_month_df(data)
+    X_mo = lamo.drop(['Year', 'Month', 'ZIP'], axis=1).values
+    W, H, labelsNMF, nmfModel = build_nmf_all(X_mo)
+    m, coords = get_basemap(LA_shapefile)
+    new_df = prep_df(lamo, W, labelsNMF)
+    for yr in xrange(2004, 2017):
+        for mo in xrange(1, 13):
+            print yr, mo
+            df_map = get_df_map(new_df[(new_df['Year'] == yr) |
+                                (new_df['Month'] == mo)], m)
+            if mo < 10:
+                title = 'img/la-map-{0}0{1}.png'.format(yr, mo)
+            else:
+                title = 'img/la-map-{0}{1}.png'.format(yr, mo)
+            info = 'Los Angeles Commuties\nCategoried by Crime\n{0}--{1}'.format(yr, mo)
+            build_map_nmf(df_map, m, coords, info, title)
+    plot_heatmap(H.T, 'img/month.png')
+    return new_df
+
+def build_map_quater(data):
+    laqt = get_quater_df(data)
+    X_qt = laqt.drop(['Year', 'Quater', 'ZIP'], axis=1).values
+    W, H, labelsNMF, nmfModel = build_nmf_all(X_qt)
+    m, coords = get_basemap(LA_shapefile)
+    new_df = prep_df(laqt, W, labelsNMF)
+    cnt = 1
+    for yr in xrange(2004, 2017):
+        for qt in xrange(1, 5):
+            print yr, qt
+            df_map = get_df_map(new_df[(new_df['Year'] == yr) |
+                                (new_df['Quater'] == qt)], m)
+            if cnt < 10:
+                title = 'img/la00{}.png'.format(cnt)
+            else:
+                title = 'img/la0{}.png'.format(cnt)
+            info = 'Los Angeles Commuties\nCategoried by Crime\nYear{0}-Qt.{1}'.format(yr, qt)
+            build_map_nmf(df_map, m, coords, info, title)
+            cnt += 1
+    plot_heatmap(H.T, 'img/quater_H.png')
+    return new_df
+
+
 if __name__ == '__main__':
     LA_shapefile = 'map/LA-ZIPCodes/geo_export_1cf2ba2c-a35e-47e7-a586-b3ff394055e9'
     ladata = pd.read_csv('data/la_clean.csv')
-    # df_map = get_df_map(new_df[new_df['Year'] == 2004])
-    # build_yr_map_nmf(df_map, m, coords, 2004)
-    build_map_yr(ladata)
-    plt.show()
+    # build_map_yr(ladata)
+    # newdf = build_map_month(ladata)
+    # build_map_month_yrbased(ladata)
+    newdf = build_map_quater(ladata)
+    # plt.show()
